1f3df7b94e0fb5105844e89d89a94c395d0b27eb
kylebaker616 <kylebaker616@gmail.com>
2021-08-29 15:23:16 -0400

    Fix heroku issue

--- constants/frontend.js
+++ constants/frontend.js
@@ -1,11 +1,7 @@
 const FRONTEND_DEV_URLS = ['https://secure-taiga-59525.herokuapp.com/']
 
 const FRONTEND_PROD_URLS = [
-<<<<<<< HEAD
-  'https://secure-taiga-59525.herokuapp.com/',
-=======
   'https://project-3-junebugs-eagle.github.io/project-3-client',
->>>>>>> b940e480dbe24e9523d9b2a091e5c620f0438a46
   'https://localhost:7165'
 ]
 
